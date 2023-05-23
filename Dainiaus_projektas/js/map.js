fdb10e8ee889cb7d88c5a2433990f7183c0d5264
DainiusFrismantas <dainius.frismantas@gmail.com>
2023-05-23 15:44:53 +0300

    Projekto pakeitimai

--- Dainiaus_projektas/js/map.js
+++ Dainiaus_projektas/js/map.js
@@ -4,20 +4,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Žemėlapis
     var mymap = L.map('map').setView([56.2, 22.35], 13);
   
-    // Kartografinis pagrindas
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 
-      '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-      minZoom: 7,
-    }).addTo(mymap);
+     // Kartografinių pagrindų sluoksnių sąrašas
+     var baseLayers = {
+      "OpenStreetMap": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+          maxZoom: 18,
+          minZoom: 7
+      }),
+      "Esri World Street Map": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri',
+        maxZoom: 18,
+        minZoom: 7
+    })
+  };
   
     // Simbolis žymintis vietą
-    var marker = L.marker([56.3, 22.35]).addTo(mymap);
+    baseLayers["Esri World Street Map"].addTo(mymap);
+    var marker = L.marker([56.303503, 22.280236]).addTo(mymap);
   
     // Iššokantis langas
     marker.bindPopup("<b>Sveiki!</b><br>Atvykite prekių apžiūrėti gyvai!").openPopup();
   
+    // Pridėti valdiklį žemėlapio kartografiniam pagrindui keisti
+    L.control.layers(baseLayers).addTo(mymap);
     // Susisiekimo forma
     var form = document.getElementById("contact-form");
   
@@ -32,4 +41,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
   
   });
+  
   
